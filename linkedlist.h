af19070dbf2a66e9688255bf1ba8927516b72988
pg0817h <31809795+pg0817h@users.noreply.github.com>
2019-04-30 22:05:13 -0700

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -7,22 +7,24 @@
 using namespace std;
 
 struct node {
-    int data;
-    node * next;
+	int data;
+	node * next;
 };
 
 class linkedlist {
 private:
-    node * head;
-    int count(); // RECURSIVE
-    int sum(); // RECURSIVE
+	node * head;
+	int count(); // RECURSIVE
+	int sum(); // RECURSIVE
 
 public:
-    linkedlist();
-    ~linkedlist(); // RECURSIVE
-    void addToFront(int n);
-    double average(); // uses sum and count functions
-    void writeInorder(string & file);
-    void writeReversed(string & file);
+	linkedlist();
+	~linkedlist(); // RECURSIVE
+	void addToFront(int n);
+	double average(); // uses sum and count functions
+	void writeInorder(string & file);
+	void writeReversed(string & file);
+	int returnData(node *temp);
+	void readData(node *temp);
 };
 #endif // end linkedlist.h
